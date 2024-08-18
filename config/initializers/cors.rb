bb6bf98158f470df83687de56cfa8ec9631a5205
yuki0877 <yuki.lucky.0927@gmail.com>
2024-08-18 19:17:18 +0900

    corsを修正

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -7,7 +7,7 @@
 
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
-    origins 'localhost:8080','https://yuki0877.github.io/Clock-Safety-Front'
+    origins 'localhost:8080','https://yuki0877.github.io'
 
     resource '*',
       headers: :any,
