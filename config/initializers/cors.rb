753cb2601f12fe72ee567152c1ae85a3abb82060
yuki0877 <yuki.lucky.0927@gmail.com>
2024-08-18 19:06:56 +0900

    github-pageのドメインを許可

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -7,7 +7,7 @@
 
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
-    origins 'localhost:8080','https://www.sample.com'
+    origins 'localhost:8080','https://yuki0877.github.io/Clock-Safety-Front'
 
     resource '*',
       headers: :any,
