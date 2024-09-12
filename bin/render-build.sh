a29685a70f34c500bbf392d30451728776c3d23f
yuki0877 <yuki.lucky.0927@gmail.com>
2024-09-12 19:58:05 +0900

    update sh

--- bin/render-build.sh
+++ bin/render-build.sh
@@ -3,6 +3,5 @@
 set -o errexit
 
 bundle install
-bundle exec rails db:migrate
 DISABLE_DATABASE_ENVIRONMENT_CHECK=1 bundle exec rake db:migrate:reset
-bundle exec rake db:migrate:seed
+bundle exec rake db:seed
